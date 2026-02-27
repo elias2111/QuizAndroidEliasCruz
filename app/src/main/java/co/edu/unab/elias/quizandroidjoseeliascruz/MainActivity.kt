d4e3438d8bb047e0abbcab60646dca70a9ebddc7
Jose Elias <jcruz895@unab.edu.co>
2026-02-27 17:40:19 -0500

    Segunda pantalla

--- app/src/main/java/co/edu/unab/elias/quizandroidjoseeliascruz/MainActivity.kt
+++ app/src/main/java/co/edu/unab/elias/quizandroidjoseeliascruz/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import co.edu.unab.elias.quizandroidjoseeliascruz.ui.theme.QuizAndroidJoseEliasCruzTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,4 +41,100 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Preview(showBackground = true)
+@Composable
+fun CiclistaScreen() {
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Perfil",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Juan Pérez", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            "Desarrollador Android apasionado por la tecnología y el diseño.",
+            fontSize = 14.sp,
+            color = Color.Gray,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("150", fontWeight = FontWeight.Bold)
+                Text("Posts")
+            }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("2.3K", fontWeight = FontWeight.Bold)
+                Text("Seguidores")
+            }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("980", fontWeight = FontWeight.Bold)
+                Text("Likes")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Seguir",
+                modifier = Modifier
+                    .background(Color(0xFF6A1B9A), shape = RoundedCornerShape(50))
+                    .padding(horizontal = 24.dp, vertical = 8.dp),
+                color = Color.White
+            )
+
+            Text(
+                "Mensaje",
+                modifier = Modifier
+                    .background(Color.LightGray, shape = RoundedCornerShape(50))
+                    .padding(horizontal = 24.dp, vertical = 8.dp)
+            )
+
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("intereses", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            "Mensaje",
+            modifier = Modifier
+                .background(Color.LightGray, shape = RoundedCornerShape(50))
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("proyectos recientes", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+    }
+
+}
